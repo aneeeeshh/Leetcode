60b18b226ca0ab71850e250dd8ca6ed774ad5349
Aneesh Gupta <142716309+aneeeeshh@users.noreply.github.com>
2023-09-30 16:19:08 +0530

    Added solution - LeetHub

---
+++ Anagram - GFG/anagram.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here  
+       int[] frequencyArray = new int[26];
+           if (a.length() != b.length()) {
+                return false;
+              }
+           for (int i = 0; i < a.length(); i++) {
+                frequencyArray[a.charAt(i) - 'a']++;
+              }
+           for (int i = 0; i < b.length(); i++) {
+                frequencyArray[b.charAt(i) - 'a']--;
+              }
+           for (int i = 0; i < 26; i++) {
+                if (frequencyArray[i] != 0) {
+                  return false;
+                 }
+              }
+             return true;
+    }
+}
